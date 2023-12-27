a841c437501d1e2f3dfcc40c2c81e08c6dcaf80c
gaeul <e0540@hanmail.net>
2023-12-27 22:08:38 +0900

    기본객체들

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,12 +1,17 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.sql.Array;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -46,6 +51,43 @@ public class BasicController {
 
         return "basic/variable";
     }
+
+    /**
+     * 스프링 3.0미만
+     */
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session){
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    /**
+     * 스프링 3.0이상
+     */
+    @GetMapping("/basic-objects2")
+    public String basicObjectes2(Model model, HttpServletRequest request, HttpServletResponse response,
+                                 HttpSession session){
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data){
+            return "Hello " + data;
+        }
+    }
+
 
     @Data
     static class User {
