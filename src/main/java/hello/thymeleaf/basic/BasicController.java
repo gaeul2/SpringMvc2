858c58b6e0d909732360ccfb62c1abbf1edb746f
gaeul <e0540@hanmail.net>
2023-12-28 22:20:37 +0900

    속성값 설정

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -100,6 +100,10 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
 
     @Component("helloBean")
     static class HelloBean {
