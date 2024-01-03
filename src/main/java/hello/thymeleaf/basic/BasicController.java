ed4dc4999b5a5d2cae2c28c6b35c1c807d0f135c
gaeul <e0540@hanmail.net>
2024-01-03 23:31:20 +0900

    section 1. 타임리프 기본기능 완료

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -130,6 +130,13 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("UserD", 20));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
 
     private void addUsers(Model model){
         List<User> list= new ArrayList<>();
